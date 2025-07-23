721437fd3495880ae420969ee8eae5b1c79a6457
P. Warren Groves Jr. <pwjrg28@gmail.com>
2025-07-22 21:21:43 -0500

    inputs posting and resetting after clicking submit

--- main.js
+++ main.js
@@ -4,5 +4,8 @@ var postText = document.getElementById("post-text");
 const btn = document.getElementById("btn");
 
 btn.addEventListener("click", function () {
-  messageBoard.insertAdjacentHTML("beforeend", `<div class="col-7 border-bottom p-2 fs-5" id="message-board">${postText.value} - Posted By: ${userName.value}</div>`)
+  messageBoard.insertAdjacentHTML("beforeend", `<div class="col-7 border-bottom p-2 fs-5" id="message-board">${postText.value} - Posted By: ${userName.value}</div>`);
+
+  userName.value = ''; 
+  postText.value = '';
 })
