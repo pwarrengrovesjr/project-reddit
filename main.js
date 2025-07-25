a9b68d85fda6e587336fcab3c5bbab093bbacf25
P. Warren Groves Jr. <pwjrg28@gmail.com>
2025-07-25 00:59:09 -0500

    Only thing left: add functionality to delete posts and comments

--- main.js
+++ main.js
@@ -1,49 +1,74 @@
-var messageBoard = document.getElementById("message-board")
-let clickCount = 0;
-var userName = document.getElementById("your-name");
-var postText = document.getElementById("post-text");
+// To do:
+// 1. Add event listener to delete posts and comments.
+
+var posts = document.getElementById("posts")
+let postCount = 0;
+var poster = document.getElementById("poster");
+var post = document.getElementById("post");
 const submitPost = document.getElementById("submitPost");
-const reply = document.getElementById("reply-thread");
 
 submitPost.addEventListener("click", function () {
-  clickCount++;
+  if (post.value === '' && poster.value === '') {
+    alert('Oops! Looks like you hit submit by accident. Make sure you add your post text and name below.')
+  } else {
+    postCount++;
+    
+    posts.insertAdjacentHTML("beforeend", `
+      <div class="col-10 border-bottom p-2 fs-5" id="post-${postCount}">
+        <button class="btn btn-primary btn-sm" type="button" data-bs-toggle="collapse" data-bs-target="#post${postCount}CommentThread" aria-expanded="false" aria-controls="commentThread"><i class="bi bi-reply"></i></button>
+        <button class="btn btn-secondary btn-sm" href=""><i class="bi bi-x"></i></button>
+        ${post.value} - Posted By: ${poster.value}
+        <div class="collapse" id="post${postCount}CommentThread">
+          <div class="row justify-content-md-center" id="post${postCount}-comments"></div>
+          <div class="row justify-content-md-center">
+            <div class="col-10 mt-3 fs-5">
+              <textarea
+                id="post${postCount}-comment"
+                class="form-control"
+                placeholder="Comment Text"
+              ></textarea>
+            </div>
+          </div>
+          <div class="row justify-content-md-center">
+            <div class="col-10 mt-3 fs-5">
+              <input
+                id="post${postCount}-commenter"
+                type="text"
+                class="form-control"
+                placeholder="Your Name"
+              />
+            </div>
+          </div>
+          <div class="row justify-content-md-center">
+            <div class="col-10 mt-3 align-self-start">
+              <button id="post${postCount}-submitComment" type="button" class="btn btn-primary">
+                Submit Comment
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>`);
   
-  messageBoard.insertAdjacentHTML("beforeend", `
-    <div class="col-7 border-bottom p-2 fs-5" id="message-${clickCount}">
-      <button class="btn btn-primary btn-sm" type="button" data-bs-toggle="collapse" data-bs-target="#commentThread" aria-expanded="false" aria-controls="commentThread"><i class="bi bi-reply"></i></button>
-      <button class="btn btn-secondary btn-sm" href=""><i class="bi bi-x"></i></button>
-      ${postText.value} - Posted By: ${userName.value}
-      <div class="collapse" id="commentThread">
-        <div class="row justify-content-md-center" id="comments"></div>
-        <div class="row justify-content-md-center">
-          <div class="col-10 mt-3 fs-5">
-            <textarea
-              id="comment-text"
-              class="form-control"
-              placeholder="Comment Text"
-            ></textarea>
-          </div>
-        </div>
-        <div class="row justify-content-md-center">
-          <div class="col-10 mt-3 fs-5">
-            <input
-              id="commenter-name"
-              type="text"
-              class="form-control"
-              placeholder="Your Name"
-            />
-          </div>
-        </div>
-        <div class="row justify-content-md-center">
-          <div class="col-10 mt-3 align-self-start">
-            <button id="submitComment" type="button" class="btn btn-primary">
-              Submit Comment
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>`);
-
-  userName.value = ''; 
-  postText.value = '';
+    poster.value = ''; 
+    post.value = '';
+  
+    var comments = document.getElementById(`post${postCount}-comments`)
+    let commentCount = 0;
+    var commenter = document.getElementById(`post${postCount}-commenter`);
+    var comment = document.getElementById(`post${postCount}-comment`);
+    const submitComment = document.getElementById(`post${postCount}-submitComment`);
+  
+    submitComment.addEventListener("click", function () {
+      commentCount++;
+      
+      comments.insertAdjacentHTML("beforeend", `
+        <div class="col-9 border-bottom p-2 fs-5" id="comment-${commentCount}">
+          <button class="btn btn-secondary btn-sm" href=""><i class="bi bi-x"></i></button>
+          ${comment.value} - Posted By: ${commenter.value}
+        </div>`);
+    
+      comment.value = ''; 
+      commenter.value = '';
+    });
+  };
 });
