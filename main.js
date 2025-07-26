8f1b7000f0e93cc053d6f77a842ad48046f7868a
P. Warren Groves Jr. <pwjrg28@gmail.com>
2025-07-26 18:36:49 -0500

    completed project - no extensions

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
-// To do:
-// 1. Add event listener to delete posts and comments.
-
 var posts = document.getElementById("posts")
+var postList = document.getElementsByClassName('post');
 let postCount = 0;
 var poster = document.getElementById("poster");
 var post = document.getElementById("post");
 const submitPost = document.getElementById("submitPost");
+
+
 
 submitPost.addEventListener("click", function () {
   if (post.value === '' && poster.value === '') {
     alert('Oops! Looks like you hit submit by accident. Make sure you add your post text and name below.')
   } else {
     postCount++;
+
     
+
     posts.insertAdjacentHTML("beforeend", `
-      <div class="col-10 border-bottom p-2 fs-5" id="post-${postCount}">
+      <div class="post col-10 border-bottom p-2 fs-5" id="post-${postCount}">
         <button class="btn btn-primary btn-sm" type="button" data-bs-toggle="collapse" data-bs-target="#post${postCount}CommentThread" aria-expanded="false" aria-controls="commentThread"><i class="bi bi-reply"></i></button>
-        <button class="btn btn-secondary btn-sm" href=""><i class="bi bi-x"></i></button>
+        <button id="delete" class="delete btn btn-secondary btn-sm"><i class="bi bi-x"></i></button>
         ${post.value} - Posted By: ${poster.value}
         <div class="collapse" id="post${postCount}CommentThread">
           <div class="row justify-content-md-center" id="post${postCount}-comments"></div>
@@ -51,7 +53,7 @@ submitPost.addEventListener("click", function () {
   
     poster.value = ''; 
     post.value = '';
-  
+
     var comments = document.getElementById(`post${postCount}-comments`)
     let commentCount = 0;
     var commenter = document.getElementById(`post${postCount}-commenter`);
@@ -62,13 +64,29 @@ submitPost.addEventListener("click", function () {
       commentCount++;
       
       comments.insertAdjacentHTML("beforeend", `
-        <div class="col-9 border-bottom p-2 fs-5" id="comment-${commentCount}">
-          <button class="btn btn-secondary btn-sm" href=""><i class="bi bi-x"></i></button>
+        <div class="comment col-9 border-bottom p-2 fs-5" id="comment-${commentCount}">
+          <button id="delete" class="delete btn btn-secondary btn-sm" href=""><i class="bi bi-x"></i></button>
           ${comment.value} - Posted By: ${commenter.value}
         </div>`);
     
       comment.value = ''; 
       commenter.value = '';
+
+      const deleteBtns = Array.from(document.getElementsByClassName('delete'));
+
+      deleteBtns.forEach(function (button) {
+        button.addEventListener('click', function () {
+          button.parentElement.remove();
+        });
+      });
+    });
+
+    const deleteBtns = Array.from(document.getElementsByClassName('delete'));
+
+    deleteBtns.forEach(function (button) {
+      button.addEventListener('click', function () {
+        button.parentElement.remove();
+      });
     });
   };
 });
